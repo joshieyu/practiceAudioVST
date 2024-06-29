3ed3655bd5a13e19582bdaacd16354e907710e5e
joshieyu <jyemails1@gmail.com>
2024-06-29 17:29:49 -0400

    Started gain plugin

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -5,18 +5,13 @@ PluginEditor::PluginEditor (PluginProcessor& p)
 {
     juce::ignoreUnused (processorRef);
 
-    addAndMakeVisible (inspectButton);
+    gainSlider.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
+    gainSlider.setRange (0.0, 1.0, 0.1);
+    gainSlider.setTextBoxStyle (juce::Slider::NoTextBox, false, 90, 0);
+    gainSlider.setTextValueSuffix (" Gain");
+    gainSlider.setValue(0.5);
 
-    // this chunk of code instantiates and opens the melatonin inspector
-    inspectButton.onClick = [&] {
-        if (!inspector)
-        {
-            inspector = std::make_unique<melatonin::Inspector> (*this);
-            inspector->onClose = [this]() { inspector.reset(); };
-        }
 
-        inspector->setVisible (true);
-    };
 
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
@@ -27,16 +22,15 @@ PluginEditor::~PluginEditor()
 {
 }
 
+void PluginEditor::sliderValueChanged(juce::Slider* slider);
+
 void PluginEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    auto area = getLocalBounds();
-    g.setColour (juce::Colours::white);
+    g.fillAll (juce::Colours::white);
+    g.setColour (juce::Colours::black);
     g.setFont (16.0f);
-    auto helloWorld = juce::String ("Hello from ") + PRODUCT_NAME_WITHOUT_VERSION + " v" VERSION + " running in " + CMAKE_BUILD_TYPE;
-    g.drawText (helloWorld, area.removeFromTop (150), juce::Justification::centred, false);
+    g.drawFittedText ("Gain", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
 }
 
 void PluginEditor::resized()
@@ -44,5 +38,4 @@ void PluginEditor::resized()
     // layout the positions of your child components here
     auto area = getLocalBounds();
     area.removeFromBottom(50);
-    inspectButton.setBounds (getLocalBounds().withSizeKeepingCentre(100, 50));
 }
--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -5,7 +5,7 @@
 #include "melatonin_inspector/melatonin_inspector.h"
 
 //==============================================================================
-class PluginEditor : public juce::AudioProcessorEditor
+class PluginEditor : public juce::AudioProcessorEditor, private juce::Slider::Listener
 {
 public:
     explicit PluginEditor (PluginProcessor&);
@@ -16,10 +16,10 @@ public:
     void resized() override;
 
 private:
+    void sliderValueChanged (juce::Slider* slider) override;
+    juce::Slider gainSlider;
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     PluginProcessor& processorRef;
-    std::unique_ptr<melatonin::Inspector> inspector;
-    juce::TextButton inspectButton { "Inspect the UI" };
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEditor)
 };
